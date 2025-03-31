35451c0f5401fd82d79f792f0a437336ca09d705
jjsv20 <jorim.svillamizar@udea.edu.co>
2025-03-31 01:00:34 -0500

    Problemas soluc

--- Lab_01/Problemas/main.cpp
+++ Lab_01/Problemas/main.cpp
@@ -8,22 +8,22 @@ int problema3();
 
 int problema5();
 
-void problema7();
+int problema7();
 
-void problema9();
+int problema9();
 
-void problema11();
+int problema11();
 
-void problema13();
+int problema13();
 
-void problema15();
+int problema15();
 
-void problema17();
+int problema17();
 
 
 int main()
 {
-    problema3();
+    problema13();
     return 0;
 }
 
@@ -85,5 +85,138 @@ int problema3()
 
 int problema5()
 {
-
+    int N, espacios, asteriscos;
+    cout << "Ingrese un numero: ";
+    cin >> N;
+    if (N % 2 == 0){
+        cout << N << " es un numero par";
+    }
+    else {
+        asteriscos = 1;
+        espacios = (N- 1)/2;
+        for (int i = 1; i <= N; i+=2){
+            for (int j = 1; j <= espacios; j++){
+                if(asteriscos != N){
+                    cout << " ";
+                }
+            }
+            for (int j = 1; j <= asteriscos; j++){
+                cout << "*";
+            }
+            for (int j = 1; j <= espacios; j++){
+                if(asteriscos != N){
+                    cout << " ";
+                }
+            }
+            cout << endl;
+            asteriscos += 2;
+            espacios --;
+        }
+        asteriscos = N - 2;
+        espacios = 1;
+        for (int i = 1; i <= N/2; i++){
+            for (int j = 1; j <= espacios; j++){
+                if(asteriscos != N){
+                    cout << " ";
+                }
+            }
+            for (int j = 1; j <= asteriscos; j++){
+                cout << "*";
+            }
+            for (int j = 1; j <= espacios; j++){
+                if(asteriscos != N){
+                    cout << " ";
+                }
+            }
+            cout << endl;
+            asteriscos -= 2;
+            espacios ++;
+        }
+    }
 }
+
+int problema7()
+{
+    int N, a=0, b=1, c=0, suma = 0;
+    cout << "Ingrese un numero: ";
+    cin >> N;
+
+    while (c < N){
+        if (c % 2 == 0){
+            suma += c;
+        }
+
+        //cout << "La serie de Fibonacci hasta " << N << " es: ";
+        cout << c << ", ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    cout << "El resultado de la suma es; " << suma << endl;
+    return 0;
+}
+
+int problema9()
+{
+    int N, suma=0, P, digito;
+    cout << "Ingrese un numeo: ";
+    cin >> N;
+    while (N>0) {
+        digito = N%10;
+        N /= 10;
+        P = 1;
+        for (int i = 0; i < digito; i++) {
+            P *= digito;
+        }
+        suma += P;
+    }
+    cout << "Suma digitos: " << suma << endl;
+    return 0;
+}
+
+int problema11()
+{
+    int N, a, b, c, mcm = 1, x;
+    cout << "Ingrese un numero: ";
+    cin >> N;
+    for(int i = 2; i <= N; i++){
+        a = mcm;
+        b=i;
+        while (b>0){
+            x = b;
+            b = a % b;
+            a = x;
+        }
+        mcm = (mcm/a)*i;
+    }
+    cout << "El minimo comun multiplo es: " << mcm <<endl;
+}
+
+int problema13()
+{
+    int N, Primo;
+    cout << "Ingrese un numero: ";
+    cin >> N;
+    for (int i = 2; i <= N; i++){
+        Primo = 1;
+        for(int j=2; j<=i/2; j++) {
+            if(i % j == 0) {
+                Primo = 0;
+            }
+        }
+
+        if(Primo == 1) {
+            cout << i << " es un número primo." << endl;
+        } else {
+            cout << i << " no es un número primo." << endl;
+        }
+
+        return 0;
+    }
+}
+
+
+
+
+
+
