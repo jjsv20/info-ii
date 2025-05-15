203747fa4677663087bed80032f2f541dbb1be93
jjsv20 <jorim.svillamizar@udea.edu.co>
2025-05-15 07:23:16 -0500

    Agregar y remover enrutadores

--- Lab_04/main.cpp
+++ Lab_04/main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <fstream>
 using namespace std;
 
 class Enrutador {
@@ -10,10 +11,23 @@ private:
     map<string, pair<string, int>> tabla; // destino -> {siguiente salto, costo}
 
 public:
+    Enrutador() : nombre("") {}
     Enrutador(string nombre) : nombre(nombre) {}
 
     string getNombre() const {
         return nombre;
+    }
+
+    void setNombre(string nuevoNombre){
+        nombre = nuevoNombre;
+    }
+
+    map<string, pair<string, int>>& getTabla(){
+        return tabla;
+    }
+
+    void setTabla(map<string, pair<string, int>> nuevaTabla) {
+        tabla = nuevaTabla;
     }
 
     void agregarRuta(string destino, string siguiente, int costo){
@@ -33,9 +47,6 @@ public:
         }
     }
 
-    map<string, pair<string, int>>& getTabla(){
-        return tabla;
-    }
 
     void actualizarTabla(map<string, pair<string, int>> nuevaTabla){
         tabla = nuevaTabla;
@@ -47,11 +58,15 @@ private:
     map<string, Enrutador> enrutadores;
 
 public:
-    void agregarEnrutador(){
-        enrutadores[enrutador.getNombre() = enrutador];
+    void agregarEnrutador(Enrutador enrutador){
+        enrutadores[enrutador.getNombre()] = enrutador;
     }
 
-    void removerEnrutador(){
+    map<string, Enrutador> getEnrutadores() const {
+        return enrutadores;
+    }
+
+    void removerEnrutador(string nombre){
         enrutadores.erase(nombre);
         for(auto& par : enrutadores) {
             par.second.eliminarRuta(nombre);
@@ -70,7 +85,7 @@ public:
             cout << "No se pudo abrir el archivo." << endl;
             return;
         }
-        string nombre, destino. siguienteSalto;
+        string nombre, destino, siguienteSalto;
         int costo;
         while (file >> nombre >> destino >> siguienteSalto >> costo) {
             if (enrutadores.find(nombre) == enrutadores.end()) {
@@ -84,48 +99,36 @@ public:
 };
 
 int main() {
-    /*/ Crear enrutadores
-    Enrutador A("A");
-    Enrutador B("B");
-
-    // Agregar rutas
-    A.actualizarRuta("B", "B", 3);
-    A.actualizarRuta("C", "B", 5);
-
-    B.actualizarRuta("A", "A", 3);
-    B.actualizarRuta("C", "C", 2);
-
-    // Mostrar tablas iniciales
-    A.mostrarTabla();
-    B.mostrarTabla();
-
-    // Eliminar una ruta
-    A.eliminarRuta("C");
-
-    cout << "\nDespués de eliminar la ruta A -> C:\n";
-    A.mostrarTabla();/*/
     Red red;
     int opcion;
     do {
-        cout << "Menu" << enld;
-        cout << "1. Agregar Enrutador" << enld;
-        cout << "2. Remover Enrutador" << enld;
+        cout << "Menu" << endl;
+        cout << "1. Agregar Enrutador" << endl;
+        cout << "2. Remover Enrutador" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Ingrese opcion: ";
         cin >> opcion;
-        string nombre, destino, siguienteSalto;
-        int costo;
+        string nombre;
         switch(opcion) {
         case 1:
             cout << "Nombre Enrutador: ";
             cin >> nombre;
-            red.agregarEnrutador((nombre));
+            red.agregarEnrutador(Enrutador(nombre));
             cout << "Enrutador agregado" << endl;
-        }
-        break;
+            break;
         case 2:
             cout << "Nombre del enrutador a eliminar: ";
             cin >> nombre;
-            red.eliminarEnrutador(nombre);
-            cout << "Enrutador eliminado.\n";
-
+            red.removerEnrutador(nombre);
+            cout << "Enrutador eliminado" << endl;
+            break;
+        case 3:
+            cout << "Saliendo..." << endl;
+            break;
+        default:
+            cout << "Opcion invalida";
+            break;
+        }
+    } while(opcion != 3);
     return 0;
 }
